0bec35314b4a5055f7629db50d085d4203e06535
unknown <samueloctavianus97@gmail.com>
2022-06-18 13:41:59 +0700

    finalizing css & adding javascript validation

--- script.js
+++ script.js
@@ -4,10 +4,32 @@ let body = document.querySelector("body");
 let overlay = document.querySelector(".menu-overlay");
 let sliderController = document.querySelector(".slider-controller");
 let sliderIndicator = document.querySelector(".testimonials__indicator");
+let btnEmailValidation = document.querySelector("#button-email-validation");
+let inputEmail = document.querySelector("#input-email");
 
+btnEmailValidation.addEventListener("click", validateInput);
+
+function validateInput(e) {
+  e.preventDefault();
+  let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+
+  if (!inputEmail.value.trim()) {
+    alert("Input can't be empty!");
+    inputEmail.classList.add("invalid");
+  } else {
+    if (regex.test(inputEmail.value)) {
+      alert("Success! Your request has been successfully submitted.");
+      inputEmail.classList.add("valid");
+    } else if (!regex.test(inputEmail.value)) {
+      inputEmail.classList.add("invalid");
+      alert("Invalid email!!");
+    }
+  }
+}
+
+//Carousel Functionality
 let slides = 4;
 let idx = 1;
-
 sliderIndicator.addEventListener("click", (e) => {
   if (e.target.classList.contains("radio")) {
     idx = +e.target.dataset.index;
@@ -26,22 +48,9 @@ sliderIndicator.addEventListener("click", (e) => {
       sliderController.style.transform = "translateX(calc(-300% - 72px))";
     }
   }
-
-  // switch (+idx) {
-  //   case 2:
-  //     sliderController.style.transform = "translateX(calc(-100% - 24px))";
-  //     break;
-  //   case 3:
-  //     sliderController.style.transform = "translateX(calc(-200% - 48px));";
-  //     break;
-  //   case 4:
-  //     sliderController.style.transform = "translateX(calc(-300% - 64px));";
-  //     break;
-  //   default:
-  //     return true;
-  // }
 });
 
+//Hamburger Toggle
 hamburgerIcon.addEventListener("click", toggleMenu);
 function toggleMenu() {
   menuContent.classList.toggle("active");
