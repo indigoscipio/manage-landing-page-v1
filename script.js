388c93da865a366e6434d52a5694cef626a00807
unknown <samueloctavianus97@gmail.com>
2022-06-18 09:23:48 +0700

    adding menu interaction

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let hamburgerIcon = document.querySelector(".header__hamburger");
 let menuContent = document.querySelector(".header__menu");
+let body = document.querySelector("body");
+let overlay = document.querySelector(".menu-overlay");
 
 hamburgerIcon.addEventListener("click", toggleMenu);
 function toggleMenu() {
   menuContent.classList.toggle("active");
+  hamburgerIcon.classList.toggle("active");
+  body.classList.toggle("locked");
+  overlay.classList.toggle("active");
 }
 
 function toggleOverlay() {
